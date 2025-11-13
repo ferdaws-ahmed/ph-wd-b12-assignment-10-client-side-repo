e0d49792d11309f562c2ccb9ffcbc0b028b399b4
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2025-11-13 18:36:32 +0600

    add a title and logo

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router'
-import router from './Routes/Routes.jsx'
-import AuthProvider from './contexts/AuthProvider.jsx'
-import { ToastContainer } from 'react-toastify'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { RouterProvider } from 'react-router';
+import router from './Routes/Routes.jsx';
+import AuthProvider from './contexts/AuthProvider.jsx';
+import { ToastContainer } from 'react-toastify';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
+
+      <RouterProvider router={router} />
       <ToastContainer position="top-center" />
     </AuthProvider>
-    
-  </StrictMode>,
-)
+  </StrictMode>
+);
